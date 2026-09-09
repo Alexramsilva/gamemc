aa1fe2bd259a24c0780d3827673dfc386cbad786
UNRC_Hub <106453707+Alexramsilva@users.noreply.github.com>
2026-09-09 17:21:37 -0600

    Rearrange title and image display in game.py

--- game.py
+++ game.py
@@ -24,8 +24,9 @@ st.set_page_config(
     page_icon="📈",
     layout="wide"
 )
-st.image("UNRC.png", caption="Universidad Nacional Rosario Castellanos", width=300)
+
 st.title("LCFI-UNRC")
+st.image("UNRC.png", caption="Universidad Nacional Rosario Castellanos", width=300)
 
 # ============================================================
 # ESTILO
